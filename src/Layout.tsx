cd51af923f03ad6bc234cf623c667f9eb7d70e9d
mobinadanshvarweb <mobinadaneshvardeveloper@gmail.com>
2024-12-09 14:24:45 +0330

    finall check

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -3,7 +3,7 @@ import SideBar from "./pages/sidebar/SideBar";
 
 const Layout = () => {
   return (
-    <div className="w-full min-h-[100vh] bg-[#E9F0EF] overflow-x-hidden relative p-20 flex font-custom ">
+    <div className="w-full min-h-[100vh] bg-[#E9F0EF] overflow-x-hidden relative p-3 lg:p-20 flex flex-wrap lg:flex-nowrap font-custom ">
       <span className="hidden lg:block w-135 h-135 -top-13 -left-24 gap-0 rounded-full  bg-custom-linear angle-0 absolute z-20 opacity-45"></span>
       <span className="hidden lg:block w-[68px] h-[68px] absolute top-[70px] left-[43px] gap-0 rounded-full bg-custom-conic-gradient shadow-custom-1 backdrop-blur-3"></span>
       <span className="hidden lg:block w-55 h-55 top-77 left-50 gap-0 rounded-full opacity-81 angle-0 absolute bg-[#42BAD4]"></span>
